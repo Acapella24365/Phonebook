870ef1b3f990164639be0283f33636cc98c84384
Acapella24365 <syedahayaamzahra@gmail.com>
2025-01-04 17:02:09 +0500

    PhoneBook

--- .py
+++ .py
@@ -39,3 +39,86 @@ def initial_phonebook():
         phone_book.append(temp)
     print(phone_book)
     return phone_book
+
+def menu():
+   print("\t\t\tSMARTPHONE DIRECTORY", flush=False)
+   print("\tYou can now perform the following operations on this phonebook\n")
+   print("1. Add a new Contact")
+   print("2. Remove an existing contact")
+   print("3. Delete all contacts")
+   print("4. Search for a contact")
+   print("5. Display all contacts")
+   print("6. Exit Phonebook")
+
+   choice = int(input("Please enter your choice: "))
+
+   return choice
+
+def add_contact(pb):
+   dip = []
+   for i in range(len(pb[0])):
+      if i == 0:
+         dip.append(str(input("Enter name:")))
+      if i == 1:
+         dip.append(str(input("Enter number:")))
+      if i == 2:
+         dip.append(str(input("Enter e-mail address:")))
+      if i == 3:
+         dip.append(str(input("Enter DOB(dd/mm/yy):")))
+      if i == 4:
+         dip.append(str(input("Enter category(Family/friends/Work/other):")))
+   
+   pb.append(dip)
+   return pb
+def remove_existing(pb):
+   query = str(input("Please enter the name of the contact you wish to remove"))
+   temp = 0
+
+   for i in range(len(pb)):
+      if query == pb[i][0]
+        temp += 1
+        print(pb.pop(i))
+        print("This query has now been removed")
+        return pb
+   if temp == 0:
+      print("Sorry you have entered an invalid query.\nPlease recheck and try again later.")
+      return pb
+
+def delete_all(pb):
+   return pb.clear
+
+def display_all(pb):
+   if not pb:
+      print("List is empty: []")
+   else:
+      for i in range(len(pb)):
+         print(pb[i])
+
+def thanks():
+   print("Thank you for using our Smartphone directory system.")
+   print("Please visit again!")
+
+   print("********************************************************************")
+
+sys.exit("Goodbye, have a nice day ahead!")
+print("....................................................................")
+
+print("Hello dear user, welcome to our smartphone directory system")
+
+print("You may now proceed to explore this directory")
+
+print("....................................................................")
+ch = 1
+pb = initial_phonebook()
+while ch in (1, 2, 3, 4, 5):
+  ch = menu()
+  if ch == 1:
+   pb = add_contact(pb)
+  elif ch == 2:
+   pb = remove_existing(pb)
+  elif ch == 3:
+   pb = delete_all(pb)
+  elif ch == 5:
+   display_all(pb)
+  else:
+   thanks()
